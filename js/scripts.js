2f79c1b7f425bd9ae45e99401b931ac30be64852
Michael Larragueta <m.larragueta@yahoo.com>
2019-04-05 09:42:29 -0700

    Add functions to add numbers needed and a function to change nums

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,37 @@
-$(function(){
+var strNum = [];
+function pushNum(num){
+  for(var i = 0; i <= num; i++){
+    strNum.push("" + i);
+  };
+};
 
+function checkNum(){
+  var holdNum = "";
+  for(var x = 0; x < strNum.length; x++){
+    holdNum = strNum[x];
+    if(holdNum.includes("3")){
+      strNum[x] = "I'm sorry, Dave. I'm afraid i can't do that";
+    }else if(holdNum.includes("2")){
+      strNum[x] = "Boop!";
+    }else if(holdNum.includes("1")){
+      strNum[x] = "Beep!";
+    };
+  };
+};
+
+
+
+
+
+
+
+$(function(){
+  $('#formOne').submit(function(event){
+      event.preventDefault();
+      var intNum = $('input#number').val();
+      pushNum(intNum);
+      checkNum();
+      console.log(strNum);
+    });
 
 });
